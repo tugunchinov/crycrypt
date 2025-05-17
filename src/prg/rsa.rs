e2ea2132db0a5dcccf012261944491c5e0f7c0f5
Maxim <tugunchinov.maxim@proton.me>
2025-05-17 20:07:55 +0300

    add statistics

--- src/prg/rsa.rs
+++ src/prg/rsa.rs
@@ -1,8 +1,8 @@
 use crate::utils::safe_power_modulo;
 use std::num::NonZeroU32;
 
-const P: u32 = 1733;
-const Q: u32 = 319919;
+const P: u32 = 58217;
+const Q: u32 = 31957;
 
 const N: NonZeroU32 = NonZeroU32::new(P * Q).unwrap();
 const PHI: u32 = (P - 1) * (Q - 1);
@@ -56,8 +56,36 @@ mod test {
         let mut prg = RsaPrg::new(seed);
 
         println!("Pseudorandom sequence:");
-        for _ in 0..10 {
+        for _ in 0..100 {
             println!("{}", prg.gen_u8());
+        }
+    }
+
+    #[test]
+    fn statistics() {
+        let seed = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .unwrap()
+            .as_millis() as u32;
+
+        let mut prg = RsaPrg::new(seed);
+
+        let mut cnts = std::collections::BTreeMap::new();
+
+        for _ in 0..1_000_000 {
+            let val = prg.gen_u8();
+            *cnts.entry(val).or_insert(0) += 1;
+        }
+
+        println!("Statistics:");
+
+        let min = cnts.values().min().unwrap();
+        let max = cnts.values().max().unwrap();
+
+        println!("Min: {}\nMax: {}", min, max);
+
+        for (val, cnt) in cnts {
+            println!("{}: {}", val, cnt);
         }
     }
 }
